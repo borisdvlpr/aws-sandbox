5279c0dbb2088c664f185b2c188bce155f115942
borisdvlpr <eduffmiranda@hotmail.com>
2024-10-29 23:51:10 +0100

    first blood on apigw-http-lambda-documentdb

---
+++ patterns/apigw-http-lambda-documentdb/test/apigw-http-lambda-documentdb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigwHttpLambdaDocumentdb from '../lib/apigw-http-lambda-documentdb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apigw-http-lambda-documentdb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigwHttpLambdaDocumentdb.ApigwHttpLambdaDocumentdbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
