540c9dae5a5917b5510ce64beeef51fff02216c3
borisdvlpr <eduffmiranda@hotmail.com>
2025-07-16 23:36:46 +0200

    chore(tofu-lambda): add infrastructure configuration for Lambda function

--- tutorials/tofu-lambda/infra/main.tf
+++ tutorials/tofu-lambda/infra/main.tf
@@ -29,3 +29,38 @@ resource "aws_dynamodb_table" "state_lock_table" {
     type = "S"
   }
 }
+
+module "rust_lambda_function" {
+  source = "terraform-aws-modules/lambda/aws"
+  version = "~> 7.20"
+
+  function_name = "hello-world-api"
+
+  handler = "bootstrap"
+  runtime = "provided.al2023"
+  architectures = ["arm64"]
+
+  trigger_on_package_timestamp = false
+
+  source_path = [
+    {
+      path = "${path.module}/../lambdas/hello-lambda"
+      commands = [
+        "cargo lambda build --release --arm64",
+        "cd target/lambda/hello-lambda",
+        ":zip",
+      ],
+      patterns = [
+        "!.*",
+        "bootstrap",
+      ]
+    }
+  ]
+  
+  create_lambda_function_url = true
+}
+
+output "function_url" {
+  value = module.rust_lambda_function.lambda_function_url
+  description = "The URL of the Lambda function"
+}
