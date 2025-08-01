aae061ac4baa7f8b0f8d6de5b6b4b5fed96d6892
borisdvlpr <eduffmiranda@hotmail.com>
2025-08-01 01:17:48 +0100

    feat(scheduled-data-summary-export): add basic lambda function structure for api lambda

--- patterns/scheduled-data-summary-export/scheduled_data_summary_export/scheduled_data_summary_export_stack.py
+++ patterns/scheduled-data-summary-export/scheduled_data_summary_export/scheduled_data_summary_export_stack.py
@@ -1,7 +1,8 @@
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway as _apigw,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
 
@@ -10,10 +11,20 @@ class ScheduledDataSummaryExportStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ScheduledDataSummaryExportQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # rest api lambda function
+        api_lambda = _lambda.Function(
+            self, 
+            "api_lambda",
+            runtime = _lambda.Runtime.NODEJS_22_X,
+            handler = "index.handler",
+            code = _lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify('Hello World!'),
+                    };
+                };
+                """
+            ),
+        )
